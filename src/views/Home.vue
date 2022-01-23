dfdc4a1285849215bbeed9f1bcd2425ca4547968
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-01-23 14:43:50 +0500

    Event Card Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 3432231,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+        },
+        {
+          id: 433443,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day 2',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'February 28, 2022',
+          time: '09:00',
+          petsAllowed: true,
+          organizer: 'Jill Laydee'
+        },
+        {
+          id: 566433,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day 3',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'March 28, 2022',
+          time: '06:00',
+          petsAllowed: true,
+          organizer: 'John Laydee'
+        }
+      ]
+    }
   }
-};
+}
 </script>
+
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
